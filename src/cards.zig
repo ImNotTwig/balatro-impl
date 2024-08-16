e7a2643b819723405d53102b203d4a83a88e6a03
ly <TwigTheCat@disroot.org>
2024-08-16 06:06:37 -0500

    first commit
    bugfixes, formatting struct field names, progress on scoreHandType()

--- src/cards.zig
+++ src/cards.zig
@@ -1,9 +1,38 @@
 const std = @import("std");
 const rand = @import("std").crypto.random;
 
-pub const Rank = enum { Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace };
-pub const Suit = enum { Diamond, Club, Spade, Heart };
+pub const Rank = enum { None, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace };
+pub const Suit = enum { None, Diamond, Club, Spade, Heart };
 pub const CardType = enum { Playing, Joker, Consumable, Voucher };
+
+pub const HandTypes = enum {
+    HighCard,
+    Pair,
+    TwoPair,
+    ThreeOak,
+    Straight,
+    Flush,
+    FullHouse,
+    FourOak,
+    StraightFlush,
+
+    // Made Up Hands below
+
+    TwoPairPlus, // a multiple of a two pair, eg four Aces and four Kings
+
+    FlushHouse, // a full house, except all cards are the same suit
+    FlushFive, // 5 cards with the same suit and rank
+    FlushOther, // 6+ cards with the same suit and rank
+
+    MultiHouse, // a hand with only 2 different ranks, with a 2/3rds ratio between them, 10n cards
+    MultiFlushHouse, // MultiHouse, except all cards are the same suit
+
+    DoubleStraight, // a straight with 2 aces
+    DoubleStraightFlush, // a straight flush with 2 aces
+
+    FiveOak, // 5 cards with the same rank
+    OtherOak, // 6+ cards with the same rank
+};
 
 pub const Edition = enum {
     None,
@@ -37,53 +66,68 @@ pub const Sticker = enum {
 };
 
 pub const Card = struct {
-    Suit: ?Suit, // A Joker or Consumable card would not have a Suit
-    Rank: ?Rank, // Nor would it have a Rank, at least normally
-    Type: CardType,
-    Enhancement: Enhancement = Enhancement.None,
-    Edition: Edition = Edition.None,
-    Seal: Seal = Seal.None,
-    Sticker: Sticker = Sticker.None,
+    suit: Suit,
+    rank: Rank,
+    type: CardType,
+    enhancement: Enhancement = Enhancement.None,
+    edition: Edition = Edition.None,
+    seal: Seal = Seal.None,
+    stickers: []Sticker = &[_]Sticker{},
+    other: struct {
+        baseValueBonus: i32 = 0,
+    } = .{},
 };
+
+pub fn getRankValue(crd: Card) i32 {
+    return (switch (crd.rank) {
+        .Ace => 11,
+        .King, .Queen, .Jack => 10,
+        else => @intFromEnum(crd.rank) + 1,
+    });
+}
 
 ///Debug Function: Printing the Suit and Rank of a Playing Card
 pub fn printVanillaCardData(card: Card) void {
     std.debug.print("{s} of {s}\n", .{
-        std.enums.tagName(Rank, card.Rank orelse undefined) orelse "",
-        std.enums.tagName(Suit, card.Suit orelse undefined) orelse "",
+        std.enums.tagName(Rank, card.Rank) orelse "",
+        std.enums.tagName(Suit, card.Suit) orelse "",
     });
 }
 
 pub const Deck = struct {
-    Cards: std.ArrayList(Card),
-    CardsLeft: std.ArrayList(Card),
+    totalCards: std.ArrayList(Card),
+    cardsLeft: std.ArrayList(Card),
     pub fn shuffleDeck(self: *Deck) !void {
         for (8) |_| {
-            for (self.Cards.items) |_| {
+            for (self.totalCards.items) |_| {
                 const i = rand.intRangeAtMost(usize, 0, 51);
-                const top_card = self.Cards.pop();
-                try self.Cards.insert(i, top_card);
+                const top_card = self.totalCards.pop();
+                try self.totalCards.insert(i, top_card);
             }
         }
     }
 };
 
 pub fn createDeck(alc: std.mem.Allocator) !Deck {
-    var d: Deck = Deck{ .Cards = std.ArrayList(Card).init(alc), .CardsLeft = std.ArrayList(Card).init(alc) };
+    var d: Deck = Deck{ .totalCards = std.ArrayList(Card).init(alc), .cardsLeft = std.ArrayList(Card).init(alc) };
 
     inline for (@typeInfo(Suit).Enum.fields) |i| {
+        if (@as(Suit, @enumFromInt(i.value)) == Suit.None) {
+            continue;
+        }
+
         inline for (@typeInfo(Rank).Enum.fields) |j| {
-            try d.Cards.append(Card{
-                .Type = CardType.Playing,
-                .Rank = @enumFromInt(j.value),
-                .Suit = @enumFromInt(i.value),
+            if (@as(Rank, @enumFromInt(j.value)) == Rank.None) {
+                continue;
+            }
+
+            try d.totalCards.append(Card{
+                .type = CardType.Playing,
+                .rank = @enumFromInt(j.value),
+                .suit = @enumFromInt(i.value),
             });
         }
     }
-    d.CardsLeft = d.Cards;
+    d.cardsLeft = d.totalCards;
     return d;
 }
-
-// export fn add(a: i32, b: i32) i32 {
-//     return a + b;
-// }
--- src/game.zig
+++ src/game.zig
@@ -4,65 +4,173 @@ const cards = @import("cards.zig");
 const SELECT_SIZE = 5;
 
 const Player = struct {
-    Deck: cards.Deck = undefined,
-    CurrentHand: struct {
-        Contents: std.ArrayList(cards.Card) = undefined,
-        Selected: std.AutoHashMap(usize, cards.Card) = undefined,
+    deck: cards.Deck = undefined,
+    currentHand: struct {
+        notSelected: std.ArrayList(cards.Card) = undefined,
+        selected: std.AutoHashMap(usize, cards.Card) = undefined,
     } = .{},
-    HandSize: i32 = 7,
-    Discards: [2]u32 = [2]u32{ 5, 5 },
-    Hands: [2]u32 = [2]u32{ 4, 4 },
+    handSize: i32 = 7,
+    discards: [2]u32 = [2]u32{ 5, 5 },
+    hands: [2]u32 = [2]u32{ 4, 4 },
 
-    Money: i32 = 7,
+    money: i32 = 7,
 
-    CurrentChips: i32 = 0,
-    CurrentMult: i32 = 0,
+    currentChips: i32 = 0,
+    currentMult: i32 = 0,
 
-    Jokers: struct {
-        Slots: [2]u32 = [2]u32{ 5, 5 },
-        Inventory: std.ArrayList(cards.Card) = undefined,
+    jokers: struct {
+        slots: [2]u32 = [2]u32{ 5, 5 },
+        inventory: std.ArrayList(cards.Card) = undefined,
     } = .{},
-    Consumables: struct {
-        Slots: [2]u32 = [2]u32{ 2, 2 },
-        Inventory: std.ArrayList(cards.Card) = undefined,
+    consumables: struct {
+        slots: [2]u32 = [2]u32{ 2, 2 },
+        inventory: std.ArrayList(cards.Card) = undefined,
     } = .{},
 
     fn selectCardInHand(self: *Player, index: usize) !void {
-        var iterator = self.CurrentHand.Selected.iterator();
-        var i: u32 = 0;
+        var iterator = self.currentHand.selected.iterator();
+        var selectedLen: u32 = 0;
         while (iterator.next()) |_| {
-            i += 1;
+            selectedLen += 1;
         }
 
-        if (i < SELECT_SIZE) {
-            try self.CurrentHand.Selected.put(index, self.CurrentHand.Contents.orderedRemove(index - i));
-        }
-    }
-    fn deselectCardInHand(self: *Player, index: usize) !void {
-        if (index > self.CurrentHand.Contents.items.len) {
-            try self.CurrentHand.Contents.insert(self.CurrentHand.Contents.items.len, self.CurrentHand.Selected.fetchRemove(index).?.value);
+        const ix: i32 = @as(i32, @intCast(index)) - @as(i32, @intCast(selectedLen)) - 1;
+
+        var adjustedIndex: usize = 0;
+        if (ix < 0) {
+            adjustedIndex = 0;
+        } else if (ix == 0) {
+            adjustedIndex = index;
+            if (ix + 1 > 0) adjustedIndex = 1;
         } else {
-            try self.CurrentHand.Contents.insert(index, self.CurrentHand.Selected.fetchRemove(index).?.value);
+            adjustedIndex = if (index <= selectedLen) index - selectedLen - 1 else index - selectedLen;
+        }
+
+        if (self.currentHand.notSelected.items.len == 0)
+            return;
+        if (selectedLen <= SELECT_SIZE and adjustedIndex > self.currentHand.notSelected.items.len)
+            return;
+
+        if (selectedLen <= SELECT_SIZE or self.currentHand.notSelected.items[adjustedIndex].edition == cards.Edition.Neg)
+            try self.currentHand.selected.put(index, self.currentHand.notSelected.orderedRemove(adjustedIndex));
+    }
+
+    fn deselectCardInHand(self: *Player, index: usize) !void {
+        const len = self.currentHand.notSelected.items.len;
+        const crd = self.currentHand.selected.fetchRemove(index).?.value;
+        if (index > len) {
+            try self.currentHand.notSelected.insert(len, crd);
+        } else {
+            try self.currentHand.notSelected.insert(index, crd);
         }
     }
 
     fn drawHand(self: *Player) !void {
-        while (self.CurrentHand.Contents.items.len + self.CurrentHand.Selected.keyIterator().len < self.HandSize) {
-            try self.CurrentHand.Contents.append(self.Deck.CardsLeft.pop());
+        while (self.currentHand.notSelected.items.len + self.currentHand.selected.keyIterator().len < self.handSize) {
+            try self.currentHand.notSelected.append(self.deck.cardsLeft.pop());
         }
     }
 
-    fn playHand(self: *Player) !void {
-        var iterator = self.CurrentHand.Selected.iterator();
+    fn playHand(self: *Player, all: std.mem.Allocator) !void {
+        try self.doScoring(all);
+        self.currentHand.selected.clearRetainingCapacity();
+        try self.drawHand();
+        std.debug.print("{any}\n", .{self.currentChips});
+    }
+
+    fn doScoring(self: *Player, all: std.mem.Allocator) !void {
+        try self.scoreHandType(all);
+
+        var iterator = self.currentHand.selected.iterator();
         while (iterator.next()) |i| {
-            self.CurrentChips += (switch (i.value_ptr.*.Rank.?) {
-                .Ace => 11,
-                .King, .Queen, .Jack => 10,
-                else => @intFromEnum(i.value_ptr.*.Rank.?) + 2,
+            const crd = i.value_ptr;
+            self.currentChips += (switch (crd.rank) {
+                .Ace => 11 + crd.other.baseValueBonus,
+                .King, .Queen, .Jack => 10 + crd.other.baseValueBonus,
+                else => @intFromEnum(crd.rank) + 1 + crd.other.baseValueBonus,
             });
-            _ = self.CurrentHand.Selected.remove(i.key_ptr.*);
+            _ = self.currentHand.selected.remove(i.key_ptr.*);
         }
-        std.debug.print("{any}\n", .{self.CurrentChips});
+    }
+
+    fn scoreHandType(self: *Player, all: std.mem.Allocator) !void {
+        var crdArrayList = std.ArrayList(cards.Card).init(all);
+
+        var iterator = self.currentHand.selected.iterator();
+        while (iterator.next()) |i| {
+            try crdArrayList.append(i.value_ptr.*);
+        }
+
+        const crdList = try crdArrayList.toOwnedSlice();
+        defer all.free(crdList);
+        std.mem.sort(cards.Card, crdList, {}, struct {
+            pub fn inner(_: void, a: cards.Card, b: cards.Card) bool {
+                return cards.getRankValue(a) < cards.getRankValue(b);
+            }
+        }.inner);
+
+        // var handType = cards.HandTypes.HighCard;
+        var canStraight = true;
+        var canDoubleStraight: i32 = 0;
+        var canFlush = true;
+        var canFullHouse = false;
+        var canTwoPair = false;
+        var canOak = std.AutoHashMap(cards.Rank, i32).init(all);
+        defer canOak.deinit();
+
+        var crdListLen: usize = 0;
+        for (0.., crdList) |i, _| {
+            crdListLen += 1;
+            _ = i;
+        }
+
+        for (0.., crdList) |i, j| {
+            if (i + 1 < crdListLen) {
+                if (crdListLen < 5) {
+                    canStraight = false;
+                    canFullHouse = false;
+                    canFlush = false;
+                }
+
+                if (j.suit != crdList[i + 1].suit) {
+                    canFlush = false;
+                }
+
+                if (j.rank == cards.Rank.Two or j.rank == cards.Rank.King or j.rank == cards.Rank.Ace) {
+                    canDoubleStraight += 1;
+                }
+                if (!(j.rank == cards.Rank.Two and crdList[crdListLen - 1].rank == cards.Rank.Ace)) {
+                    if (!(@intFromEnum(j.rank) + 1 == @intFromEnum(crdList[i + 1].rank))) {
+                        if (!(@intFromEnum(j.rank) + 1 == @intFromEnum(cards.Rank.Ace)))
+                            canStraight = false;
+                    }
+                }
+            }
+
+            if (canOak.getEntry(j.rank)) |v| {
+                try canOak.put(j.rank, v.value_ptr.* + 1);
+            } else {
+                try canOak.put(j.rank, 1);
+            }
+        }
+
+        var oakIterator = canOak.valueIterator();
+        var pointSixCheck = false;
+        var twoCardCheck: i32 = 0;
+        while (oakIterator.next()) |i| {
+            const float_i = @as(f32, @floatFromInt(i.*));
+            if (@mod(float_i, 3) == 0) {
+                pointSixCheck = true;
+            } else if (@mod(float_i, 2) == 0) {
+                twoCardCheck += 1;
+            }
+        }
+        if (twoCardCheck == 1 and pointSixCheck) {
+            canFullHouse = true;
+        }
+        if (twoCardCheck == 2) {
+            canTwoPair = true;
+        }
     }
 };
 
@@ -72,61 +180,43 @@ const Game = struct {
 
 test "Card Selection" {
     var p = Player{
-        .Deck = try cards.createDeck(std.testing.allocator),
-        .CurrentHand = .{
-            .Selected = std.AutoHashMap(usize, cards.Card).init(std.testing.allocator),
-            .Contents = std.ArrayList(cards.Card).init(std.testing.allocator),
+        .deck = try cards.createDeck(std.testing.allocator),
+        .currentHand = .{
+            .selected = std.AutoHashMap(usize, cards.Card).init(std.testing.allocator),
+            .notSelected = std.ArrayList(cards.Card).init(std.testing.allocator),
         },
     };
-    defer p.Deck.Cards.deinit();
-    defer p.CurrentHand.Contents.deinit();
-    defer p.CurrentHand.Selected.deinit();
+    defer p.deck.totalCards.deinit();
+    defer p.currentHand.notSelected.deinit();
+    defer p.currentHand.selected.deinit();
 
-    try p.Deck.shuffleDeck();
+    try p.deck.shuffleDeck();
+    try p.currentHand.notSelected.append(cards.Card{ .rank = cards.Rank.Ace, .suit = cards.Suit.Club, .type = cards.CardType.Playing });
+    try p.currentHand.notSelected.append(cards.Card{ .rank = cards.Rank.Ace, .suit = cards.Suit.Club, .type = cards.CardType.Playing });
+    try p.currentHand.notSelected.append(cards.Card{ .rank = cards.Rank.Three, .suit = cards.Suit.Club, .type = cards.CardType.Playing });
+    try p.currentHand.notSelected.append(cards.Card{ .rank = cards.Rank.Three, .suit = cards.Suit.Club, .type = cards.CardType.Playing });
     try p.drawHand();
-
-    for (p.CurrentHand.Contents.items) |i| {
-        std.debug.print("{?} - ", .{i.Rank});
-        std.debug.print("{?} | Contents\n", .{i.Suit});
-    }
-    std.debug.print("\n\n", .{});
-    var iterator = p.CurrentHand.Selected.iterator();
-    while (iterator.next()) |i| {
-        std.debug.print("{any} - ", .{i.value_ptr.*.Rank});
-        std.debug.print("{any} | Selected\n", .{i.value_ptr.*.Suit});
-    }
-
+    try std.testing.expect(p.currentHand.notSelected.items.len == 7);
+    // try p.CurrentHand.Contents.append(cards.Card{ .Rank = cards.Rank.Three, .Suit = cards.Suit.Club, .Type = cards.CardType.Playing });
     try p.selectCardInHand(0);
     try p.selectCardInHand(1);
     try p.selectCardInHand(2);
     try p.selectCardInHand(3);
-    try p.selectCardInHand(4);
+    // try p.selectCardInHand(4);
     try p.selectCardInHand(5);
-    try p.selectCardInHand(6);
+    // try p.selectCardInHand(6);
 
-    for (p.CurrentHand.Contents.items) |i| {
-        std.debug.print("{?} - ", .{i.Rank});
-        std.debug.print("{?} | Contents\n", .{i.Suit});
+    for (p.currentHand.notSelected.items) |i| {
+        std.debug.print("{any} - ", .{i.rank});
+        std.debug.print("{any} | Contents\n", .{i.suit});
     }
     std.debug.print("\n\n", .{});
-    iterator = p.CurrentHand.Selected.iterator();
+    var iterator = p.currentHand.selected.iterator();
     while (iterator.next()) |i| {
-        std.debug.print("{any} - ", .{i.value_ptr.*.Rank});
-        std.debug.print("{any} | Selected\n", .{i.value_ptr.*.Suit});
+        std.debug.print("{any} - ", .{i.value_ptr.rank});
+        std.debug.print("{any} - ", .{i.value_ptr.suit});
+        std.debug.print("{any} | Selected\n", .{i.key_ptr.*});
     }
 
-    try p.deselectCardInHand(2);
-
-    try p.playHand();
-
-    for (p.CurrentHand.Contents.items) |i| {
-        std.debug.print("{?} - ", .{i.Rank});
-        std.debug.print("{?} | Contents\n", .{i.Suit});
-    }
-    std.debug.print("\n\n", .{});
-    iterator = p.CurrentHand.Selected.iterator();
-    while (iterator.next()) |i| {
-        std.debug.print("{any} - ", .{i.value_ptr.*.Rank});
-        std.debug.print("{any} | Selected\n", .{i.value_ptr.*.Suit});
-    }
+    try p.playHand(std.testing.allocator);
 }
